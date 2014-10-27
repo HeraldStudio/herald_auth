d7599d7ce9386f1c44c94f4fd4f61a02d8a21ab1
telotic <telotic@gmail.com>
2014-10-27 17:15:35 +0900

    A little completion

--- mod/UpdateHandler.py
+++ mod/UpdateHandler.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Date    : 2014-10-26 16:10:46
-# @Author  : yml_bright@163.com
 
 import tornado.web
 from sqlalchemy.sql import and_
@@ -17,12 +15,30 @@ class UpdateHandler(tornado.web.RequestHandler):
         return self.application.db
 
     def get(self):
-        self.write('<form method="post"><input type="text" name="appid"><input type="text" name="uuid"><input type="submit" name="submit"></form>')
+        #uuid和appid两项用以在数据库中比对确认修改用户信息的权限
+        #其他项留空则不修改原数据
+        self.write('''<form method="post">
+                        <p>凭据信息</p>
+                        <p>uuid            <input type="text" name="uuid"></p>
+                        <p>appid           <input type="text" name="appid"></p>
+                        <HR>
+                        <p>更新用户信息（留空不变动）</p>
+                        <p>cardnum(不可变动)</p>
+                        <p>number          <input type="text" name="number"></p>
+                        <p>password        <input type="password" name="password"></p>
+                        <p>pe_password     <input type="password" name="pe_password"></p>
+                        <p>lib_username    <input type="text" name="lib_username"></p>
+                        <p>lib_password    <input type="password" name="lib_password"></p>
+                        <p>card_query_pwd  <input type="password" name="card_query_pwd"></p>
+                        <p>card_consume_pwd<input type="password" name="card_consume_pwd"></p>
+                        <p><input type="submit" name="submit"></p>
+                    </form>''')
         self.write('Herald Auth.')
         self.finish()
 
     def post(self):
-        cardnum = self.get_argument('cardnum', default='')
+        uuid = self.get_argument('uuid', default = '')
+        appid = self.get_argument('appid', default = '')
         number = self.get_argument('number', default='')
         password = self.get_argument('password', default='')
         pe_password = self.get_argument('pe_password', default='')
@@ -32,11 +48,45 @@ class UpdateHandler(tornado.web.RequestHandler):
         card_consume_pwd = self.get_argument('card_consume_pwd', default='')
 
         try:
+            #通过uuid和appid检验权限
             app = self.db.query(Application).filter(
                 Application.uuid == appid).one()
             pri = self.db.query(Privilege).filter(
                 and_(Privilege.uuid == uuid, Privilege.aid == app.aid)).one()
-            self.write('OK')
+            #通过uuid选择对应的用户
+            user = self.db.query(User).filter(
+                User.cardnum == pri.cardnum).one()
+            cardnum = user.cardnum
+            #非空则更改用户信息
+            change_times = 0;
+            if number:
+                user.number = number
+                change_times = change_times + 1
+            if password:
+                user.password = password
+                change_times = change_times + 1
+            if pe_password:
+                user.pe_password = pe_password
+                change_times = change_times + 1
+            if lib_username:
+                user.lib_username = lib_username
+                change_times = change_times + 1
+            if lib_password:
+                user.lib_password = lib_password
+                change_times = change_times + 1
+            if card_query_pwd:
+                user.card_query_pwd = card_query_pwd
+                change_times = change_times + 1
+            if card_consume_pwd:
+                user.card_consume_pwd = card_consume_pwd
+                change_times = change_times + 1
+
+            self.db.add(user)
+            self.db.commit()
+
+            self.write('<p>修改成功，%d项更新。</p>'%(change_times))
+            self.write('Herald Auth.')
+            self.finish()
         except NoResultFound:
             raise tornado.web.HTTPError(401)
         self.finish()
