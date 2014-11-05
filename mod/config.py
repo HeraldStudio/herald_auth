92aee724fc33a8ed8ebe323b453193fef692cd29
ymlbright <yml_bright@163.com>
2014-11-05 13:24:42 +0800

    sync

--- mod/config.py
+++ mod/config.py
@@ -3,7 +3,7 @@
 # @Date    : 2014-10-26 16:54:01
 # @Author  : yml_bright@163.com
 
-API_URL = 'http://127.0.0.1:7005/service/'
-CONNECT_TIME_OUT = 4
+API_URL = 'http://127.0.0.1/webserv2/'
+CONNECT_TIME_OUT = 7
 TERM = '14-15-2'
 
